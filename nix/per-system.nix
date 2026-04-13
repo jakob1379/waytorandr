6dbab9ad358a0f0a13d120685e4eab52bad5755f
Jakob Guldberg Aaes <jakob1379@gmali.com>
2026-04-13 14:01:19 +0200

    refactor: split flake package outputs into modules

---
+++ nix/per-system.nix
@@ -0,0 +1,40 @@
+{
+  self,
+  nixpkgs,
+  rust-overlay,
+  system,
+  workspace,
+}:
+
+let
+  overlays = [ rust-overlay.overlays.default ];
+  pkgs = import nixpkgs { inherit system overlays; };
+  context = import ./package-context.nix {
+    inherit self pkgs system workspace;
+  };
+  packages = import ./release-packages.nix {
+    inherit pkgs workspace;
+    inherit (context)
+      distroPortableRoot
+      flatpakPortableRoot
+      isPortableHost
+      lib
+      portablePackage
+      snapPortableRoot
+      waytorandrPackage
+      ;
+  };
+  devShell = import ./dev-shell.nix {
+    inherit pkgs;
+    inherit (context)
+      devShellBuildInputs
+      devShellTools
+      runtimeLibraries
+      rust
+      ;
+  };
+in
+{
+  inherit devShell;
+  packages = packages // { default = packages.waytorandr; };
+}
